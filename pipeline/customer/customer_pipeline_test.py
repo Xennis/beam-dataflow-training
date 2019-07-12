562b82b386eb3946fe1b1ed09a02dd42b209270c
Xennis <code@xennis.org>
2019-07-12 22:53:42 +0200

    Fix error list contains a list

--- pipeline/customer/customer_pipeline_test.py
+++ pipeline/customer/customer_pipeline_test.py
@@ -34,7 +34,7 @@ class TestCustomerPipeline(testutil.FileTestCase):
 
             expected_output = [
                 '{"errors": [], "first_name": "Bart", "id": 1, "last_name": "Bruck", "sum": "287.69"}\n',
-                '{"errors": [["email \'wtuppeny2bandcamp.com\' is invalid"]], "first_name": "Winny", "id": 3, "last_name": "Tuppeny", "sum": "1075.90"}\n',  # nopep8, pylint: disable=line-too-long
+                '{"errors": ["email \'wtuppeny2bandcamp.com\' is invalid"], "first_name": "Winny", "id": 3, "last_name": "Tuppeny", "sum": "1075.90"}\n',  # nopep8, pylint: disable=line-too-long
                 '{"errors": ["no customer details"], "first_name": null, "id": 2, "last_name": null, "sum": "2016.75"}\n'  # nopep8, pylint: disable=line-too-long
             ]
             self.assertFileEqual(FileSystems.join(test_dir, 'output-*-of-*.json'), expected_output)
